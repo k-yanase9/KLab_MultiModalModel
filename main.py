f85f822be1f09664cd20412ef9ca201c5d469780
Absolute-Value <ji.kei1220@gmail.com>
2023-05-25 03:12:23 +0900

    言語特徴を出せるように

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import argparse
+import sys
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 # ArgumentParserオブジェクトを作成
 parser = argparse.ArgumentParser(description='このスクリプトの説明')
-parser.add_argument('--model_name', type=str, choices=['t5-11b', 't5-3b', 't5-small', 't5-base', 't5-large'], default='t5-11b')
+parser.add_argument('--model_name', type=str, choices=['t5-11b', 't5-3b', 't5-small', 't5-base', 't5-large'], default='t5-large')
 args = parser.parse_args()
 
 print("Loading model...")
-tokenizer = T5Tokenizer.from_pretrained(args.model_name, model_max_length=1024)
+tokenizer = T5Tokenizer.from_pretrained(args.model_name, model_max_length=512)
 model = T5ForConditionalGeneration.from_pretrained(args.model_name)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 model = model.to(device)
+print("model loaded")
 
-def translate_text(text):
-    input_text = "translate Englist to Japanse: " + text
-    input_ids = tokenizer.encode(input_text, return_tensors="pt").to(device)
-    translated = model.generate(input_ids=input_ids, max_length=512, num_beams=4, early_stopping=True)
-    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
-    return translated_text
-
-response = "AI: Please enter English sentence."
+response = "AI: Please enter sentence."
 print(response)
 while True:
     print("You: ", end="")
     input_text = input()
     if input_text == 'end':
         break
-    response = translate_text(input_text)
-    print(f"T5: {response}")
+    
+    # Tokenize the input text
+    inputs = tokenizer.encode_plus(
+        input_text,
+        add_special_tokens=True,
+        return_tensors='pt'
+    ).to(device)
+
+    # Generate language features
+    with torch.no_grad():
+        outputs = model.encoder(inputs['input_ids'])
+
+    # Extract the language features
+    language_features = outputs.last_hidden_state.squeeze(0)
+
+    print(language_features)
+    print(language_features.shape)
+    print(sys.getsizeof(input_text), sys.getsizeof(language_features))
