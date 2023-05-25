c47e28b9c75c161f077750b119c68cc2db689c54
Absolute-Value <ji.kei1220@gmail.com>
2023-05-25 20:33:46 +0900

    Update models

--- modules/models.py
+++ modules/models.py
@@ -3,7 +3,7 @@ from torch import nn
 from transformers import AutoTokenizer, AutoImageProcessor, T5EncoderModel, Swinv2Model, T5ForConditionalGeneration
 
 # モデルの定義
-class FeatureExtractor(nn.Module):
+class MyModel(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.device = args.device
@@ -14,31 +14,21 @@ class FeatureExtractor(nn.Module):
         self.tokenizer = AutoTokenizer.from_pretrained(args.language_model_name, model_max_length=512)
         self.language_model = T5EncoderModel.from_pretrained(args.language_model_name).to(self.device) # device_map="auto"
 
-    def forward(self, images, src_texts):
-        images = self.image_processor(images, return_tensors="pt").to(self.device)
-
-        image_embeddings = self.image_model(**images).last_hidden_state
-
-        srcs = self.tokenizer(src_texts, return_tensors='pt', padding=True).to(self.device) # ['pt', 'tf', 'np', 'jax']
-        language_embeddings = self.language_model(srcs['input_ids']).last_hidden_state
-
-        concated_embeddings = torch.cat((image_embeddings,language_embeddings), dim=1)
-
-        return concated_embeddings
-    
-class MyTransformer(nn.Module):
-    def __init__(self, args):
-        super().__init__()
-        self.device = args.device
-
-        self.tokenizer = AutoTokenizer.from_pretrained(args.language_model_name, model_max_length=512)
         self.transformer = T5ForConditionalGeneration.from_pretrained(args.language_model_name).to(self.device)
         self.loss_function = nn.CrossEntropyLoss()
 
-    def forward(self, concated_embeddings, tgt_texts):
-        # outputs = self.transformer(concated_embeddings)
+    def forward(self, images, src_texts, tgt_texts):
+        with torch.no_grad():
+            images = self.image_processor(images, return_tensors="pt").to(self.device)
+
+            image_embeddings = self.image_model(**images).last_hidden_state
+
+            srcs = self.tokenizer(src_texts, return_tensors='pt', padding=True).to(self.device) # ['pt', 'tf', 'np', 'jax']
+            language_embeddings = self.language_model(srcs['input_ids']).last_hidden_state
+
+            concated_embeddings = torch.cat((image_embeddings,language_embeddings), dim=1)
+
         tgts = self.tokenizer(tgt_texts, return_tensors='pt', padding=True).to(self.device) # ['pt', 'tf', 'np', 'jax']
-        # loss = self.loss_function(outputs, tgts['input_ids'])
 
         loss = self.transformer(inputs_embeds=concated_embeddings, labels=tgts['input_ids']).loss
 
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn.parallel import DataParallel
 from tqdm import tqdm
 
 from modules.config import parse_arguments
@@ -11,9 +12,8 @@ args = parse_arguments()
 args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # モデルの読み込み
-extractor_model = FeatureExtractor(args)
-trasformer_model = MyTransformer(args)
-optimizer = torch.optim.Adam(trasformer_model.parameters(), lr=args.lr)
+model = MyModel(args)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
 # データローダーの設定
 train_dataset = DatasetLoader(args, phase="train")
@@ -21,15 +21,22 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_
 val_dataset = DatasetLoader(args, phase="val")
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=2, pin_memory=True, shuffle=False, drop_last=False)
 
-# 学習ループ
 for epoch in range(args.num_epochs):
+    # 学習ループ
     train_loop = tqdm(train_loader)
     for images, src_texts, tgt_texts in train_loop:
-        with torch.no_grad():
-            concated_embeddings = extractor_model(images, src_texts)
-        loss = trasformer_model(concated_embeddings, tgt_texts)
+        loss = model(images, src_texts, tgt_texts)
 
         # 勾配の計算とパラメータの更新
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+    # 検証ループ
+    val_losses = []
+    for images, src_texts, tgt_texts in val_loader:
+        with torch.no_grad():
+            loss = model(images, src_texts, tgt_texts)
+            val_losses.append(loss)
+
+    print(f'{epoch+1}: {torch.mean(val_losses)}')
