db3be131390eacd4e5a8603060fef68c86f3ee07
Absolute-Value <ji.kei1220@gmail.com>
2023-05-26 13:50:28 +0900

    Update config

--- modules/config.py
+++ modules/config.py
@@ -2,12 +2,11 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='プログラムの説明')
-    parser.add_argument('--local-rank', type=int)
     parser.add_argument('--image_model_name', type=str, default="microsoft/swinv2-base-patch4-window8-256", help='画像の特徴抽出モデル')
     parser.add_argument('--language_model_name', type=str, default="t5-large", help='言語の特徴抽出モデル')
     parser.add_argument('--lr', type=float, default=0.001, help='学習率')
     parser.add_argument('--data_dir', type=str, default='/data/dataset/v-coco/coco/', help='データのディレクトリ')
-    parser.add_argument('--batch_size', type=int, default=128, help='バッチサイズ')
+    parser.add_argument('--batch_size', type=int, default=128, help='1GPUあたりのバッチサイズ')
     parser.add_argument('--num_epochs', type=int, default=100, help='エポック数')
     parser.add_argument('--max_source_length', type=int, default=256)
     parser.add_argument('--max_target_length', type=int, default=128)
