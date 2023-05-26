b7d537244c5d4edbb0087eb9495fe4f96586099a
Absolute-Value <ji.kei1220@gmail.com>
2023-05-26 13:14:08 +0900

    公式通り（動作しない）

--- modules/models.py
+++ modules/models.py
@@ -1,36 +1,25 @@
 import torch
 from torch import nn
-from transformers import AutoTokenizer, AutoImageProcessor, T5EncoderModel, Swinv2Model, T5ForConditionalGeneration
+from transformers import T5EncoderModel, Swinv2Model, T5ForConditionalGeneration, logging
+logging.set_verbosity_error()
 
 # モデルの定義
 class MyModel(nn.Module):
     def __init__(self, args):
         super().__init__()
-        self.device = args.device
-        self.max_source_length = 256
-        self.max_target_length = 128
         
-        self.image_processor = AutoImageProcessor.from_pretrained(args.image_model_name)
-        self.image_model = Swinv2Model.from_pretrained(args.image_model_name).to(self.device)
+        self.image_model = Swinv2Model.from_pretrained(args.image_model_name)
 
-        self.tokenizer = AutoTokenizer.from_pretrained(args.language_model_name, model_max_length=512)
-        self.language_model = T5EncoderModel.from_pretrained(args.language_model_name).to(self.device) # device_map="auto"
+        self.language_model = T5EncoderModel.from_pretrained(args.language_model_name) # device_map="auto"
 
-        self.transformer = T5ForConditionalGeneration.from_pretrained(args.language_model_name).to(self.device)
+        self.transformer = T5ForConditionalGeneration.from_pretrained(args.language_model_name)
         self.loss_function = nn.CrossEntropyLoss()
 
-    def forward(self, images, src_texts, tgt_texts):
+    def forward(self, images, source_encoding, target_encoding):
         with torch.no_grad():
-            images = self.image_processor(images, return_tensors="pt").to(self.device)
-
             image_embeddings = self.image_model(**images).last_hidden_state
-
-            source_encoding = self.tokenizer(src_texts, padding="longest", max_length=self.max_source_length, return_tensors='pt').to(self.device) # ['pt', 'tf', 'np', 'jax']
             language_embeddings = self.language_model(source_encoding['input_ids']).last_hidden_state
-
             concated_embeddings = torch.cat((image_embeddings,language_embeddings), dim=1)
-
-        target_encoding = self.tokenizer(tgt_texts, padding="longest", max_length=self.max_target_length, return_tensors='pt').to(self.device) # ['pt', 'tf', 'np', 'jax']
 
         loss = self.transformer(inputs_embeds=concated_embeddings, labels=target_encoding['input_ids']).loss
 
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import os
 import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+from transformers import AutoImageProcessor, AutoTokenizer
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
@@ -7,23 +11,29 @@ from modules.config import parse_arguments
 from modules.models import *
 from modules.loader import DatasetLoader
 
-def train():
+def train(rank, world_size):
+    # create default process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
     args = parse_arguments()
-    os.makedirs(args.result_dir, exist_ok=True)
+    if rank == 0:
+        os.makedirs(args.result_dir, exist_ok=True)
 
-    # デバイスをGPUに設定
-    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    os.environ["TOKENIZERS_PARALLELISM"] = "true"
+    # create local model
+    model = MyModel(args).to(rank)
+    # construct DDP model
+    model = DDP(model, device_ids=[rank])
+    # define optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-    # モデルの読み込み
-    model = MyModel(args)
-    optimizer = torch.optim.Adam(model.transformer.parameters(), lr=args.lr)
+    image_processor = AutoImageProcessor.from_pretrained(args.image_model_name)
+    tokenizer = AutoTokenizer.from_pretrained(args.language_model_name, model_max_length=512)
 
     # データローダーの設定
     train_dataset = DatasetLoader(args, phase="train")
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=2, pin_memory=True, shuffle=True, drop_last=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=os.cpu_count(), pin_memory=True, shuffle=True, drop_last=True)
     val_dataset = DatasetLoader(args, phase="val")
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=2, pin_memory=True, shuffle=False, drop_last=False)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=os.cpu_count(), pin_memory=True, shuffle=False, drop_last=False)
 
     min_val_loss = 100
     val_loss_list = []
@@ -32,7 +42,10 @@ def train():
         train_loop = tqdm(train_loader, desc=f'Train (Epoch {epoch+1}/{args.num_epochs})')
         model.train()
         for images, src_texts, tgt_texts in train_loop:
-            loss = model(images, src_texts, tgt_texts)
+            images = image_processor(images, return_tensors="pt").to(rank)
+            source_encoding = tokenizer(src_texts, padding="longest", max_length=args.max_source_length, return_tensors='pt').to(rank) # ['pt', 'tf', 'np', 'jax']
+            target_encoding = tokenizer(tgt_texts, padding="longest", max_length=args.max_target_length, return_tensors='pt').to(rank) # ['pt', 'tf', 'np', 'jax']
+            loss = model(images, source_encoding, target_encoding)
 
             # 勾配の計算とパラメータの更新
             optimizer.zero_grad()
@@ -41,30 +54,47 @@ def train():
 
         # 検証ループ
         val_losses = []
+        model.eval()
+
         val_loop = tqdm(val_loader, desc=f'Val (Epoch {epoch+1}/{args.num_epochs})')
-        for images, src_texts, tgt_texts in val_loader:
+        for images, src_texts, tgt_texts in val_loop:
             with torch.no_grad():
                 loss = model(images, src_texts, tgt_texts)
                 val_losses.append(loss)
 
-        val_loss = torch.mean(torch.tensor(val_losses))
-        val_loss_list.append(val_loss)
-        model.eval()
-        if val_loss < min_val_loss:
-            min_val_loss = val_loss
-            model.save(args.result_dir)
-        print(f'{epoch+1}: {val_loss}')
+        if rank == 0:
+            val_loss = torch.mean(torch.tensor(val_losses))
+            val_loss_list.append(val_loss)
+        
+            if val_loss < min_val_loss:
+                min_val_loss = val_loss
+                model.save(args.result_dir)
+                print('Model saved')
+            print(f'{epoch+1}: {val_loss}')
 
-    # Plot the loss values.
-    plt.plot(val_loss_list)
+    if rank == 0:
+        # Plot the loss values.
+        plt.plot(val_loss_list)
 
-    # Set the title and axis labels.
-    plt.title('Val Loss Curve')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
+        # Set the title and axis labels.
+        plt.title('Val Loss Curve')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
 
-    # Show the plot.
-    plt.savefig(f"{args.result_dir}/val_loss.png")
+        # Show the plot.
+        plt.savefig(f"{args.result_dir}/val_loss.png")
+
+def main():
+    world_size = 4
+    mp.spawn(train,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True)
 
 if __name__=="__main__":
-    train()
+    # Environment variables which need to be
+    # set when using c10d's default "env"
+    # initialization mode.
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    main()
