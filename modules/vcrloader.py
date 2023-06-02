126bf4e43850e6d5fc3055504be5dcf947793a5c
k-yanase <k-yanase@cv.info.gifu-u.ac.jp>
2023-06-02 12:09:53 +0900

    add vcrloaderを作成

---
+++ modules/vcrloader.py
@@ -0,0 +1,106 @@
+import os
+import json
+import re
+import random
+from copy import deepcopy
+from PIL import Image
+import torch
+from torchvision.transforms import ToTensor,functional
+import time
+import tqdm
+import torch.distributed as dist
+
+_GENDER_NEUTRAL_NAMES = ['Casey', 'Riley', 'Jessie', 'Jackie', 'Avery', 'Jaime', 'Peyton', 'Kerry', 'Jody', 'Kendall',
+                        'Skyler', 'Frankie', 'Pat', 'Quinn', 'Harper', 'Michael', 'Logan', 'Noah', 'James', 'Evelyn',
+                        'Avery', 'Madison', 'Riley', 'Jayden', 'Ainsley', 'Arden', 'Dakota', 'Finley', 'Hayden', 'Lennox',
+                        'Lindsey','Robin','Parker' 'River', 'Rowan', 'Avey', 'Jordan', 'Cameron', 'Angel', 'Carter','Ryan',
+                        'Dylan', 'Ezra', 'Emery', 'Hunter', 'Kai', 'Nova', 'Ollie']
+
+class Vcrdataset(torch.utils.data.Dataset):
+    def __init__(self,data_dir,mode="vqa",split="train",imagesize=(256,256)):
+        self.mode = mode
+        self.split = split
+        self.data_dir = data_dir
+        self.transform = ToTensor()
+        self.imagesize = imagesize
+
+        with open(os.path.join(self.data_dir,"vcr1annots",f"{self.split}.jsonl"),"r") as f:
+            self.items = [json.loads(s) for s in f]
+
+
+    def _make_text(self,source_txt,objects,is_q=True):
+        for idx,txt in enumerate(source_txt):
+            if type(txt)==list: #オブジェクトはリストで与えられる
+                source_txt[idx] = ""
+                for obj in txt:
+                    if is_q:
+                        source_txt[idx] += f"loc{objects[obj]['bbox'][0]} loc{objects[obj]['bbox'][1]} loc{objects[obj]['bbox'][2]} loc{objects[obj]['bbox'][3]} {objects[obj]['name']}, " 
+                    else:
+                        source_txt[idx] += objects[obj]['name'] + ", "
+                source_txt[idx] = source_txt[idx][:-2]
+        return " ".join(source_txt)
+    
+    def _get_boxes(self,box,xrasio,yrasio):
+        x1,y1,x2,y2,_ = box
+        x1 = int(x1*xrasio)
+        x2 = int(x2*xrasio)
+        y1 = int(y1*yrasio)
+        y2 = int(y2*yrasio)
+        
+        return (x1,y1,x2,y2)
+
+    def __getitem__(self,idx):
+        item= deepcopy(self.items[idx])
+        image = Image.open(os.path.join(f'{self.data_dir}',"vcr1images",f'{item["img_fn"]}')).convert("RGB").resize(self.imagesize)
+        image = self.transform(image)
+        question = item["question"]
+        answer = item["answer_choices"][item["answer_label"]]
+        namelist = random.sample(_GENDER_NEUTRAL_NAMES,item["objects"].count('person'))
+        with open(os.path.join(f'{self.data_dir}',"vcr1images",f'{item["metadata_fn"]}')) as f:
+            metadata = json.load(f)
+        xrasio = self.imagesize[0]/metadata["width"]
+        yrasio = self.imagesize[0]/metadata["height"]
+        objects = []
+        per_count = 0
+        for index,obj in enumerate(item["objects"]):
+            bbox = self._get_boxes(metadata["boxes"][index],xrasio,yrasio)
+            if obj=="person":
+                objects.append({"name":namelist[per_count],"bbox":bbox})
+                per_count+=1
+            else:
+                objects.append({"name":obj,"bbox":bbox})
+        
+        src_text = self._make_text(question,objects)
+        tgt_text = self._make_text(answer,objects,is_q=False)
+        return image,src_text,tgt_text,objects
+
+    def __len__(self):
+        return len(self.items)
+
+def get_dataloader(args, phase, rank):
+    dataset = Vcrdataset(args.data_dir, split=phase)
+    sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=torch.cuda.device_count(), rank=rank, shuffle=True)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=os.cpu_count()//4, pin_memory=True, sampler=sampler,collate_fn=lambda x: x)
+    return dataloader
+
+if __name__ =="__main__":
+    _DATADIR = "/data/dataset/vcr"
+    import argparse
+    parser = argparse.ArgumentParser(description='ローカル検証用')
+    parser.add_argument('--batch_size', type=int, default=64, help='1GPUあたりのバッチサイズ')
+    parser.add_argument('--data_dir', type=str, default=_DATADIR, help='データのディレクトリ')
+    args = parser.parse_args()
+    dist.init_process_group("nccl")
+    rank = dist.get_rank()
+    dataloader = get_dataloader(args,"train",rank)
+    # dataset = Vcrdataset(_DATADIR)
+    # dataloader = torch.utils.data.DataLoader(dataset,batch_size=16,num_workers=5,collate_fn=lambda x: x)
+    
+    # for index in range(40000,len(dataset)):
+    #     data = dataset[index]       
+    #     print(f"i:{index},q:{data[1]},a:{data[2]}")
+    
+    with open("./result.txt","a") as f:
+        for i,batch in enumerate(tqdm.tqdm(dataloader)):
+            for data in batch:
+                f.write(f"q:{data[1]},a:{data[2]} \n")
