1d4f1272d69f3d411f018c83fb0d44bb6370844c
Absolute-Value <ji.kei1220@gmail.com>
2023-06-09 21:45:30 +0900

    schedulerを変更

--- modules/scheduler.py
+++ modules/scheduler.py
@@ -1,13 +1,15 @@
 from torch.optim.lr_scheduler import *
 
 def get_scheduler(args, optimizer):
-    if args.lr_scheduler == 'cosine':
-        return CosineAnnealingLR(optimizer, T_max=args.num_epochs, eta_min=0)
-    elif args.lr_scheduler == 'linear':
-        return LambdaLR(optimizer, lr_lambda=lambda epoch: 1 - (epoch / args.num_epochs))
-    elif args.lr_scheduler == 'exponential':
+    if args.lr_scheduler == 'CosineAnnealingLR':
+        return CosineAnnealingLR(optimizer, T_max=args.num_epochs, eta_min=args.lr/100)
+    elif args.lr_scheduler == 'ExponentialLR':
         return ExponentialLR(optimizer, gamma=0.9)
-    elif args.lr_scheduler == 'step':
+    elif args.lr_scheduler == 'StepLR':
         return StepLR(optimizer, step_size=10, gamma=0.1)
+    elif args.lr_scheduler == 'MultiStepLR':
+        return MultiStepLR(optimizer, milestones=[args.num_epochs//2, args.num_epochs*3//4, args.num_epochs*7//8], gamma=0.5)
+    elif args.lr_scheduler == 'LambdaLR':
+        return LambdaLR(optimizer, lr_lambda=lambda epoch: 0.99 ** epoch)
     else:
         return None
