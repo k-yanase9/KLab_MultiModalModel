a11b42ba2ec91999c8b48dabe2e260a92374fc55
k-yanase <k-yanase@cv.info.gifu-u.ac.jp>
2023-06-09 10:46:06 +0900

    fix datasetを直接取れるように変更

---
+++ data/__init__.py
@@ -0,0 +1,1 @@
+from .get_loader import get_dataloader,get_dataset
--- data/get_loader.py
+++ data/get_loader.py
@@ -5,6 +5,14 @@ from .vcr import Vcrdataset
 from .vqa2 import Vqa2dataset
 
 def get_dataloader(args, phase, rank):
+    dataset = get_dataset(args,phase)
+    
+    sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=torch.cuda.device_count(), rank=rank, shuffle=True, drop_last=True)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=os.cpu_count()//4, pin_memory=True, sampler=sampler)
+    return dataloader
+
+
+def get_dataset(args, phase):
     if 'mscoco' in args.data_dir.lower():
         dataset = COCODatasetLoader(args.data_dir, phase)
     elif 'redcaps' in args.data_dir.lower():
@@ -15,7 +23,4 @@ def get_dataloader(args, phase, rank):
         dataset = Vqa2dataset(args.data_dir,phase=phase)
     else:
         raise NotImplementedError
-    
-    sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=torch.cuda.device_count(), rank=rank, shuffle=True, drop_last=True)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=os.cpu_count()//4, pin_memory=True, sampler=sampler)
-    return dataloader
+    return dataset
