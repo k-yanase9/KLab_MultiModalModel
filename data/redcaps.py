4699bc44fc1fd9e0a6c003c265b21de1717e1d46
Absolute-Value <ji.kei1220@gmail.com>
2023-06-15 15:48:11 +0900

    redcapsを手動でtrain/val分割

--- data/redcaps.py
+++ data/redcaps.py
@@ -5,19 +5,20 @@ from PIL import Image
 from .dataset_loader import DatasetLoader
 
 class RedCapsDatasetLoader(DatasetLoader):
-    def __init__(self, data_dir='/data/dataset/redcaps', phase='train'):
+    def __init__(self, data_dir='/data/dataset/redcaps'):
         super().__init__()
         anno_dir = os.path.join(data_dir, 'removed_annotations')
         img_dir = os.path.join(data_dir, 'images')
 
         for annotations_file_name in os.listdir(anno_dir):
-            annotations_filepath = os.path.join(anno_dir, annotations_file_name)
-            annotations = json.load(open(annotations_filepath))
-            
-            for ann in annotations["annotations"]:
-                img_path = os.path.join(img_dir, ann["subreddit"], f"{ann['image_id']}.jpg")
-                self.images.append(img_path)
-                self.src_texts.append(ann['raw_caption'])
+            if "2020" in annotations_file_name: # 2020年のデータのみ使用
+                annotations_filepath = os.path.join(anno_dir, annotations_file_name)
+                annotations = json.load(open(annotations_filepath))
+                
+                for ann in annotations["annotations"]:
+                    img_path = os.path.join(img_dir, ann["subreddit"], f"{ann['image_id']}.jpg")
+                    self.images.append(img_path)
+                    self.src_texts.append(ann['raw_caption'])
 
     def __getitem__(self, idx):
         image, src_text = self.images[idx], self.src_texts[idx]
