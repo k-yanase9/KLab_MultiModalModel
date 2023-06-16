9f70c393e3cbed519bc589826a7deb84b995ccc2
Absolute-Value <ji.kei1220@gmail.com>
2023-06-16 20:21:47 +0900

    imagenetでクラス分類ができるように

---
+++ run_scripts/image_classify/train_only_transformer.sh
@@ -0,0 +1,13 @@
+for model in "google/flan-t5-small"
+do
+torchrun --nnodes=1 --nproc_per_node=4 train.py \
+        --language_model_name google/flan-t5-base \
+        --ffn \
+        --transformer_model_name $model \
+        --lr 0.001 \
+        --optimizer AdamW \
+        --num_epochs 50 \
+        --save_interval 50 \
+        --data_dir /data/dataset/imagenet_2012/ \
+        --result_dir results/image_classify/only_transformer/$model/
+done
