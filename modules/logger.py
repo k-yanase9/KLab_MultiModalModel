0928423a572672e9f6bcb008489bebbd75e5399d
Absolute-Value <ji.kei1220@gmail.com>
2023-06-20 04:37:37 +0900

    logの名前を変更できるように

--- modules/logger.py
+++ modules/logger.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-def get_logger(args):
+def get_logger(args, log_name='train.log'):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s: %(message)s')
@@ -13,7 +13,7 @@ def get_logger(args):
     logger.addHandler(sh)
 
     # ログのファイル出力先を設定
-    fh = logging.FileHandler(os.path.join(args.result_dir, 'train.log'), mode='w')
+    fh = logging.FileHandler(os.path.join(args.result_dir, log_name), mode='w')
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
