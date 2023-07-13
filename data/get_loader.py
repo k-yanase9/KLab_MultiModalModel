e87b24a2f1a3c3e375d806fe837ce3fa10b110e6
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-07-13 11:20:36 +0900

    分割seed固定

--- data/get_loader.py
+++ data/get_loader.py
@@ -11,7 +11,9 @@ def get_data(args):
         val_size = int(len(dataset) * val_rate)
         train_size = len(dataset) - val_size
 
-        train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+        train_dataset, val_dataset = torch.utils.data.random_split(
+            dataset, [train_size, val_size], generator=torch.Generator().manual_seed(args.seed)
+        )
         
     else:
         train_dataset = get_dataset(args, phase="train")
@@ -21,13 +23,15 @@ def get_data(args):
     
 def get_dataloader(args, dataset):
     sampler = torch.utils.data.distributed.DistributedSampler(dataset, drop_last=True)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=3, pin_memory=True, sampler=sampler)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=16, pin_memory=True, sampler=sampler)
     return dataloader
 
 def get_dataset(args, phase="train"):
     if args.pretrain: # 事前学習だったら
         if 'redcaps' in args.data_dir.lower():
             dataset = RedCapsPretrainDatasetLoader(args.data_dir)
+        elif 'imagenet' in args.data_dir.lower():
+            dataset = ImageNetPretrainDatasetLoader(args.data_dir)
         else:
             raise NotImplementedError
     else:
