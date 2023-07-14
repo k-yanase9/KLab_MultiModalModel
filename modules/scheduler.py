406dc90606f7dde0d9174ba9cee1ef2640b2d9f4
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-07-14 19:05:53 +0900

    Schedulerを拡張

--- modules/scheduler.py
+++ modules/scheduler.py
@@ -1,4 +1,5 @@
 from torch.optim.lr_scheduler import *
+from transformers import get_linear_schedule_with_warmup, get_cosine_schedule_with_warmup
 
 def get_scheduler(args, optimizer):
     if args.lr_scheduler == 'CosineAnnealingLR':
@@ -6,10 +7,14 @@ def get_scheduler(args, optimizer):
     elif args.lr_scheduler == 'ExponentialLR':
         return ExponentialLR(optimizer, gamma=0.9)
     elif args.lr_scheduler == 'StepLR':
-        return StepLR(optimizer, step_size=10, gamma=0.1)
+        return StepLR(optimizer, step_size=10, gamma=0.5)
     elif args.lr_scheduler == 'MultiStepLR':
         return MultiStepLR(optimizer, milestones=[args.num_epochs//2, args.num_epochs*3//4, args.num_epochs*7//8], gamma=0.5)
     elif args.lr_scheduler == 'LambdaLR':
         return LambdaLR(optimizer, lr_lambda=lambda epoch: 0.99 ** epoch)
+    elif args.lr_scheduler == 'LinearWarmup':
+        return get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=args.num_steps)
+    elif args.lr_scheduler == 'CosineWarmup':
+        return get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=args.num_steps)
     else:
         return None
