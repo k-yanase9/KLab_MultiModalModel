e36d6c4d8d01d51f2b892e89b3e5a2a79f1f4b23
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-07-21 14:48:43 +0900

    places365のsrc_textを調整

--- data/pretrain/places365.py
+++ data/pretrain/places365.py
@@ -16,7 +16,8 @@ class Places365PretrainDatasetLoader(Places365):
         ])
 
     def __getitem__(self, index):
-        image, src_text = super().__getitem__(index)
+        image, target = super().__getitem__(index)
+        src_text = 'a photo of ' + target.split('/')[2].replace('_', ' ')
         tgt_text = ''
 
         src_image = self.src_transforms(image)
