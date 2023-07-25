26511ee07e6356057eb5432a4cadb353cdd26a45
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-07-25 17:52:39 +0900

    optimizerの引数model変更

--- modules/optimizer.py
+++ modules/optimizer.py
@@ -2,8 +2,8 @@ from torch.optim import *
 
 def get_optimizer(model, args):
     if args.optimizer == 'Adam':
-        return Adam(model.module.transformer.parameters(), lr=args.lr)
+        return Adam(model.parameters(), lr=args.lr)
     elif args.optimizer == 'AdamW':
-        return AdamW(model.module.transformer.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-8)
+        return AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-8)
     else:
         raise NotImplementedError
