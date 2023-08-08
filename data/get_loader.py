a699f70e4ff5baf0e5afc1d5e999435b2cb157a7
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-08-09 02:23:27 +0900

    sun397を対応

--- data/get_loader.py
+++ data/get_loader.py
@@ -62,7 +62,7 @@ def get_dataset(args, dataset_name, phase="train", src_tokenizer=None, tgt_token
             elif phase == "val":
                 dataset = Places365PretrainDatasetLoader(root=data_dir, split="val", small=True, src_tokenizer=src_tokenizer, tgt_tokenizer=tgt_tokenizer)
         elif 'sun397' == dataset_name:
-            dataset = SUN397PretrainDatasetLoader(root=data_dir, src_tokenizer=src_tokenizer, tgt_tokenizer=tgt_tokenizer)
+            dataset = SUN397PretrainDatasetLoader(args, data_dir, src_tokenizer=src_tokenizer, tgt_tokenizer=tgt_tokenizer)
         elif 'inaturalist' == dataset_name:
             if phase == "train":
                 dataset = INaturalistPretrainDatasetLoader(root=data_dir, version=f'2021_train', src_tokenizer=src_tokenizer, tgt_tokenizer=tgt_tokenizer)
