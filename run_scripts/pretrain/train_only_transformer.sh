ada7619d03050a543fbb39829a2ab7a575b0487f
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-08-10 22:02:50 +0900

    モデルをbaseの設定に変更

--- run_scripts/pretrain/train_only_transformer.sh
+++ run_scripts/pretrain/train_only_transformer.sh
@@ -1,24 +1,29 @@
-batch_size=128
-dataset="redcaps places365 sun397 inaturalist"
+batch_size=256
+dataset="sun397"
 
-for enc in 4
-do
-        for dec in 4
-        do
-        torchrun --nnodes=1 --nproc_per_node=4 train.py \
-                --language_model_name google/flan-t5-base \
-                --image_model_name microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft \
-                --ffn \
-                --transformer_num_layers $enc \
-                --transformer_num_decoder_layers $dec \
-                --pretrain \
-                --lr 0.01 \
-                --optimizer AdamW \
-                --lr_scheduler StepLR \
-                --batch_size $batch_size \
-                --num_epochs 50 \
-                --root_dir /local/ \
-                --dataset $dataset \
-                --result_dir results/pretrain/redcaps_places365_sun397_inaturalist/enc$enc\_dec$dec/
-        done
-done
+d_model=768
+d_ff=3072
+d_kv=64
+num_heads=12
+enc=2
+dec=12
+
+torchrun --nnodes=1 --nproc_per_node=4 train.py \
+        -l google/flan-t5-large \
+        -i microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft \
+        --ffn \
+        --transformer_d_model $d_model \
+        --transformer_d_ff $d_ff \
+        --transformer_d_kv $d_kv \
+        --transformer_num_heads $num_heads \
+        --transformer_num_layers $enc \
+        --transformer_num_decoder_layers $dec \
+        --pretrain \
+        --lr 0.01 \
+        --optimizer AdamW \
+        --lr_scheduler StepLR \
+        -b $batch_size \
+        --num_epochs 50 \
+        --root_dir /local/ \
+        --dataset $dataset \
+        --result_dir results/pretrain/$dataset/enc$enc\_dec$dec/
