1f7fb247e0ac90f4ccec8ddcaeeb6468bdc17447
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-08-10 22:51:51 +0900

    redcapsの全データを使用

--- data/pretrain/redcaps.py
+++ data/pretrain/redcaps.py
@@ -9,11 +9,11 @@ class RedCapsPretrainDatasetLoader(PretrainDatasetLoader):
         img_dir = os.path.join(data_dir, 'images')
 
         for annotations_file_name in os.listdir(anno_dir):
-            if "2019" in annotations_file_name or "2020" in annotations_file_name: # 2019年のデータのみ使用
-                annotations_filepath = os.path.join(anno_dir, annotations_file_name)
-                annotations = json.load(open(annotations_filepath))
-                
-                for ann in annotations["annotations"]:
-                    img_path = os.path.join(img_dir, ann["subreddit"], f"{ann['image_id']}.jpg")
-                    self.images.append(img_path)
-                    self.src_texts.append(ann['raw_caption'])
+            # if "2019" in annotations_file_name or "2020" in annotations_file_name: # 2019年のデータのみ使用
+            annotations_filepath = os.path.join(anno_dir, annotations_file_name)
+            annotations = json.load(open(annotations_filepath))
+            
+            for ann in annotations["annotations"]:
+                img_path = os.path.join(img_dir, ann["subreddit"], f"{ann['image_id']}.jpg")
+                self.images.append(img_path)
+                self.src_texts.append(ann['raw_caption'])
