97758edaa9dac0a3792fb3ecb5e43c2528d61e23
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-08-16 16:22:47 +0900

    cc12mフォルダ名を修正

--- data/pretrain/cc12m.py
+++ data/pretrain/cc12m.py
@@ -4,13 +4,13 @@ from .pretrain import PretrainDatasetLoader
 class CC12MPretrainDatasetLoader(PretrainDatasetLoader):
     def __init__(self, args, data_dir='/data/datatset/cc12m', resize=256, src_tokenizer=None, tgt_tokenizer=None, mask_probability=0.15):
         super().__init__(args, resize, src_tokenizer, tgt_tokenizer, mask_probability)
-        tsv_path = os.path.join(data_dir, 'train.tsv')
+        tsv_path = os.path.join(data_dir, 'alldata.tsv')
 
         with open(tsv_path, 'r') as f:
             lines = f.readlines()
 
         for line in lines[1:]:
             img_name, caption = line.removesuffix('\n').split('\t')
-            img_path = os.path.join(data_dir, 'train', img_name)
+            img_path = os.path.join(data_dir, 'images', img_name)
             self.images.append(img_path)
             self.src_texts.append(caption)
