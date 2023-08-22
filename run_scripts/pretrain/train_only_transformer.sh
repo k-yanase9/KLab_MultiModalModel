2ccc166d4d6a49bcafeef6dbcfad1061709d285f
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-08-22 15:31:50 +0900

    メインの事前学習用shell

--- run_scripts/pretrain/train_only_transformer.sh
+++ run_scripts/pretrain/train_only_transformer.sh
@@ -1,5 +1,5 @@
-batch_size=256
-dataset="sun397"
+batch_size=50
+dataset="cc3m cc12m imagenet inaturalist places365 redcaps sun397"
 
 d_model=768
 d_ff=3072
@@ -8,7 +8,7 @@ num_heads=12
 enc=2
 dec=12
 
-torchrun --nnodes=1 --nproc_per_node=4 train.py \
+torchrun --nnodes=1 --nproc_per_node=8 train.py \
         -l google/flan-t5-large \
         -i microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft \
         --ffn \
@@ -23,7 +23,7 @@ torchrun --nnodes=1 --nproc_per_node=4 train.py \
         --optimizer AdamW \
         --lr_scheduler StepLR \
         -b $batch_size \
-        --num_epochs 50 \
-        --root_dir /local/ \
+        --num_epochs 20 \
+        --root_dir /user/data/ \
         --dataset $dataset \
-        --result_dir results/pretrain/$dataset/enc$enc\_dec$dec/
+        --result_dir results/pretrain/cc3m_cc12m_imagenet_inaturalist_places365_redcaps_sun397/enc$enc\_dec$dec/
