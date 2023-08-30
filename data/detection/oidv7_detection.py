333abe7d4d96020f824929e902287b83479e3e80
k-yanase <k-yanase@cv.info.gifu-u.ac.jp>
2023-08-30 19:57:04 +0900

    fix データセット周りの修正

--- data/detection/oidv7_detection.py
+++ data/detection/oidv7_detection.py
@@ -4,66 +4,35 @@ from PIL import Image
 from copy import deepcopy
 import torch
 from torchvision.transforms import ToTensor
+from ..dataset_loader import DatasetLoader
 
 #存在しない画像を除外するためのリスト
 dropimageidlist = ["7f1934f5884fad79"]
 
-class OpenImageDataset_detection(torch.utils.data.Dataset):
+class OpenImageDataset_detection(DatasetLoader):
     """openimageのdetectionデータセット
     """    
     def __init__(self,data_dir:str="/data/dataset/openimage/",phase:str="train",imagesize:tuple[int,int]=(256,256)):
-        
+        super().__init__()        
         if phase=="val":
-            self.phase = "validation"
-        else:
-            self.phase = phase
+            phase = "validation"
 
-        self.data_dir = data_dir
-        self.imagesize = imagesize
-        self.transform = ToTensor()
+        with open(os.path.join(data_dir,"bbox",f"{phase}_detection_40.csv")) as f:
+            items = f.read()
 
-        datapath = os.path.join(data_dir,"bbox",f"{self.phase}_detection_40.csv")
-        self.df = pd.read_csv(datapath)
-        #dropimageidlistに含まれる画像を除外する
-        self.df = self.df[self.df["imageID"].isin(dropimageidlist)==False]
-        leabelpath = os.path.join(data_dir,"oidv7-class-descriptions.csv")
-        self.labels = pd.read_csv(leabelpath)
-    
-    # def _return_loc(self,imsize:tuple[int,int],bbox: list[float,float,float,float])->tuple[int,int,int,int]:
-    #     """locationを返す
+        items = items.split("\n")
+        items = [item.split(",") for item in items]
+        items = items[1:-1]
+        self.tgt_texts = [item[1] for item in items]
+        self.src_texts = ["What objects are in the image?"]*len(items)
+        self.images = [os.path.join(data_dir,phase,f"{item[0]}.jpg") for item in items]
 
-    #     Parameters
-    #     ----------
-    #     imsize : tuple[int,int]
-    #         画像のサイズ
-    #     bbox : list[float,float,float,float]
-    #         bboxの情報
-
-    #     Returns
-    #     -------
-    #     x1,x2,y1,y2 : int,int,int,int
-    #         locationのタプル
-    #     """        
-    #     x1 = int(imsize[0]*bbox[0])
-    #     x2 = int(imsize[0]*bbox[2])
-    #     y1 = int(imsize[1]*bbox[1])
-    #     y2 = int(imsize[1]*bbox[3])
-    #     return x1,x2,y1,y2
-        
-    def __getitem__(self,index):
-        data = self.df.iloc[index]
-        imagepath = os.path.join(self.data_dir,self.phase,data["imageID"]+".jpg")
-        image = self.transform(Image.open(imagepath).convert("RGB").resize(self.imagesize))
-        src_text = "What objects are in the image?"
-        tgt_text = data["text"]
-        return image,src_text,tgt_text
-    
-    def __len__(self):
-        return len(self.df)
-
-
-if __name__ =="__main__":
-    dataset = OpenImageDataset_detection(data_dir="/local_data1/openimage",phase="test")
-    data = dataset[0]
-    print(data)
+        #dropimageidlistに含まれる画像と対応するテキストを除外する
+        for drop_id in dropimageidlist:
+            drop_path = os.path.join(data_dir,phase,f"{drop_id}.jpg")
+            if drop_path in self.images:
+                drop_index = self.images.index(drop_path)
+                self.tgt_texts.pop(drop_index)
+                self.src_texts.pop(drop_index)
+                self.images.pop(drop_index)
     
--- data/vqa/vqa2.py
+++ data/vqa/vqa2.py
@@ -1,37 +1,12 @@
 import json
-from copy import deepcopy
-from PIL import Image
-import torch
-from torchvision.transforms import ToTensor,functional
+from ..dataset_loader import DatasetLoader
 
-
-
-class Vqa2dataset(torch.utils.data.Dataset):
+class Vqa2dataset(DatasetLoader):
     def __init__(self,data_dir="/data/dataset/vqa2",phase="train",imagesize=(256,256)):
-        self.phase = phase
-        self.data_dir = data_dir
-        self.transform = ToTensor()
-        self.imagesize = imagesize
+        super().__init__()
+        quetions = json.load(open(f'{data_dir}/v2_OpenEnded_mscoco_{phase}2014_questions.json'))
+        answers = json.load(open(f'{data_dir}/v2_mscoco_{phase}2014_annotations.json'))
 
-        self.quetions = json.load(open(f'{self.data_dir}/v2_OpenEnded_mscoco_{self.phase}2014_questions.json'))
-        self.answers = json.load(open(f'{self.data_dir}/v2_mscoco_{self.phase}2014_annotations.json'))
-        
-    def __getitem__(self,idx):
-        src_text = self.quetions['questions'][idx]["question"]
-        tgt_text = self.answers['annotations'][idx]["multiple_choice_answer"]
-        imgbase='%s/%s/COCO_%s_%012d.jpg'
-        imgid = self.quetions['questions'][idx]["image_id"]
-        imgpath = imgbase%(self.data_dir,f"{self.phase}2014", f"{self.phase}2014", imgid)
-        image = Image.open(imgpath).convert("RGB").resize(self.imagesize)
-        image = self.transform(image)
-        return image,src_text,tgt_text
-
-    def __len__(self):
-        return len(self.quetions["questions"])
-
-if __name__ =="__main__":
-    _DATADIR = "/data/dataset/vqa2"
-    dataset = Vqa2dataset(_DATADIR)
-    # data = dataset[10]
-    print(len(dataset))
-    # print(data)
+        self.src_texts = [item["question"] for item in quetions['questions']]
+        self.tgt_texts = [item["multiple_choice_answer"] for item in answers['annotations']]
+        self.images = [f'{data_dir}/{phase}2014/COCO_{phase}2014_{str(item["image_id"]).zfill(12)}.jpg' for item in quetions['questions']]
