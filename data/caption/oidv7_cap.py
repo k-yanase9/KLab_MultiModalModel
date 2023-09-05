abc0adb4d0cc66010163257be86f5e4b897a1409
k-yanase <k-yanase@cv.info.gifu-u.ac.jp>
2023-09-05 10:48:34 +0900

    check openimagedatasetの中身チェック

--- data/caption/oidv7_cap.py
+++ data/caption/oidv7_cap.py
@@ -6,7 +6,8 @@ from torchvision.transforms import ToTensor
 from ..dataset_loader import DatasetLoader
 
 #存在しない画像を除外するためのリスト
-dropimageidlist = ["7f1934f5884fad79"]
+dropimageidlist =["7f1934f5884fad79","429019e83c1c2c94","4f818c006da84c9e","5b86e93f8654118a","673d74b7d39741c3","6dcd3ce37a17f2be","805baf9650a12710"
+                   ,"98ac2996fc46b56d","a46a248a39f2d97c"]
 
 class OpenImageDataset_Caption(DatasetLoader):
     def __init__(self,data_dir="/data/dataset/openimage",phase="train",imagesize=(256,256)):
@@ -19,11 +20,11 @@ class OpenImageDataset_Caption(DatasetLoader):
 
         self.tgt_texts = [item["caption"] for item in self.items]
         self.src_texts = ["What does the image describe?"]*len(self.items)
-        self.images = [os.path.join(data_dir,phase,f"{item['image_id']}.jpg") for item in self.items]
+        self.images = [os.path.join(data_dir,f"{phase}_256",f"{item['image_id']}.jpg") for item in self.items]
 
         #dropimageidlistに含まれる画像と対応するキャプションを除外する
         for drop_id in dropimageidlist:  
-            drop_path = os.path.join(data_dir,phase,f"{drop_id}.jpg")
+            drop_path = os.path.join(data_dir,f"{phase}_256",f"{drop_id}.jpg")
             if drop_path in self.images:
                 drop_index = self.images.index(drop_path)
                 self.tgt_texts.pop(drop_index)
