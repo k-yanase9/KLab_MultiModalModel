10e4a0a2a3ca6753c36066fb065a84f90a60337a
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-09-23 03:22:02 +0900

    shellの更新

--- run_scripts/caption/train_openimage_caption.sh
+++
@@ -1,7 +0,0 @@
-torchrun --nnodes=1 --nproc_per_node=4 train.py \
- --data_dir /data/dataset/openimage  \
- --image_model_train \
- --result_dir results/caption/oid \
- --batch_size 8 \
- --num_epochs 30 \
- --save_interval 5
--- run_scripts/localization/train_one.sh
+++ run_scripts/localization/train_one.sh
@@ -1,29 +1,19 @@
-batch_size=2
+batch_size=1
 dataset="openimage"
 
-d_model=768
-d_ff=3072
-d_kv=64
-num_heads=12
 enc=2
 dec=0
 
-CUDA_VISIBLE_DEVICES=2 python train_one.py \
-        -l google/flan-t5-large \
-        -i microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft \
-        --ffn \
-        --transformer_d_model $d_model \
-        --transformer_d_ff $d_ff \
-        --transformer_d_kv $d_kv \
-        --transformer_num_heads $num_heads \
+python train_one.py \
         --transformer_num_layers $enc \
         --transformer_num_decoder_layers $dec \
+        --phase classify \
         --loss CrossEntropy \
         --lr 0.01 \
-        --optimizer AdamW \
         --lr_scheduler StepLR \
         -b $batch_size \
+        --start_epoch 1 \
         --num_epochs 100 \
-        --root_dir /local/ \
-        --dataset $dataset \
+        --datasets $dataset \
+        --root_dir /data/dataset/ \
         --result_dir results/localization/$dataset/enc$enc\_dec$dec/
