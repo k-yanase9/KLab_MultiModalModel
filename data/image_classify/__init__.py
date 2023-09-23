3ca00d28855770678f99231e2abadf6e448e3ec2
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-09-23 11:59:07 +0900

    rename imagenet21k

--- data/image_classify/__init__.py
+++ data/image_classify/__init__.py
@@ -1,3 +1,3 @@
 from .imagenet import ImageNetDatasetLoader
-from .imagenet_21k import ImageNet_21k_DatasetLoader
+from .imagenet21k import ImageNet21k_DatasetLoader
 from .sun397 import SUN397DatasetLoader
