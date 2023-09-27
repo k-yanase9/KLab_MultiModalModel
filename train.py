2e41a505abbfd95051819490e064b7d4b6f033ea
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-09-27 23:53:26 +0900

    不老対応版

--- train.py
+++ train.py
@@ -13,14 +13,26 @@ from modules import *
 from models.model import MyModel
 
 def train():
-    dist.init_process_group("nccl")
-    rank = dist.get_rank()
-
     args = parse_arguments()
-    args.gpu_nums = torch.cuda.device_count() # GPU数
-    device_id = rank % args.gpu_nums
+    if False:
+        port_num = 27971
+        host_list_file = os.environ["PJM_O_NODEINF"]
+        args.world_size = int(os.environ["OMPI_COMM_WORLD_SIZE"])
+        world_rank = int(os.environ["OMPI_COMM_WORLD_RANK"])
+        local_rank = int(os.environ['OMPI_COMM_WORLD_LOCAL_RANK'])
+        with open(host_list_file) as f:
+            host = f.readlines()
+        host[0] = host[0].rstrip("\n")
+        dist_url = "tcp://" + host[0] + ":" + str(port_num)
+        dist.init_process_group(backend="nccl", init_method=dist_url, rank=world_rank, world_size=args.world_size)
+    else:
+        dist.init_process_group(backend="nccl")
+        args.world_size = torch.cuda.device_count() # GPU数
+        world_rank = dist.get_rank()
+        local_rank = world_rank % args.world_size
+        dist_url = "env://"
 
-    if rank == 0: os.makedirs(args.result_dir, exist_ok=True)
+    if world_rank == 0: os.makedirs(args.result_dir, exist_ok=True)
 
     random.seed(args.seed)
     np.random.seed(args.seed)
@@ -28,13 +40,13 @@ def train():
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
-    if rank == 0: logger = get_logger(args)
+    if world_rank == 0: logger = get_logger(args)
 
     # create model
-    model = MyModel(args).to(device_id)
+    model = MyModel(args).to(local_rank)
     if args.start_epoch > 1:
         model.load(result_name='best.pth')
-    model = DDP(model, device_ids=[device_id])#,find_unused_parameters=True)
+    model = DDP(model, device_ids=[local_rank])#,find_unused_parameters=True)
     
     optimizer = get_optimizer(model, args)
     scheduler = get_scheduler(args, optimizer)
@@ -65,7 +77,7 @@ def train():
                     loss_counter.add("val", float(line.split(',')[2].split(':')[-1].strip()))
                     steps = int(line.split(',')[3].split(':')[-1].strip())
         min_val_loss = min(loss_counter.losses['val'])
-        if rank == 0: logger.info(f'[Loaded] steps : {steps}, Best Val loss : {min_val_loss}')
+        if world_rank == 0: logger.info(f'[Loaded] steps : {steps}, Best Val loss : {min_val_loss}')
     else:
         steps = 0
         min_val_loss = 100
@@ -75,27 +87,27 @@ def train():
         if args.language_model_train: model.module.language_model.train()
         if args.image_model_train: model.module.image_model.train()
         model.module.transformer.train()
-        train_loss = torch.tensor(0.0).to(device_id)
-        if args.phase == 'classify': train_acc = torch.tensor(0.0).to(device_id)
-        train_count = torch.tensor(0).to(device_id)
-        pbar = tqdm(total=int(np.ceil(len(train_loader)/args.accumulation_steps)), desc=f'Train (Epoch {epoch}/{args.num_epochs})', disable=(rank != 0))
+        train_loss = torch.tensor(0.0).to(local_rank)
+        if args.phase == 'classify': train_acc = torch.tensor(0.0).to(local_rank)
+        train_count = torch.tensor(0).to(local_rank)
+        pbar = tqdm(total=int(np.ceil(len(train_loader)/args.accumulation_steps)), desc=f'Train (Epoch {epoch}/{args.num_epochs})', disable=(world_rank != 0))
         for i, (src_images, tgt_images, src_texts, tgt_texts) in enumerate(train_loader):
             if i % args.accumulation_steps == 0:
                 optimizer.zero_grad()
-            src_images = src_images.to(device_id, non_blocking=True)
+            src_images = src_images.to(local_rank, non_blocking=True)
             # if args.phase == 'pretrain':
-            #     tgt_images = tgt_images.to(device_id)
+            #     tgt_images = tgt_images.to(local_rank)
             #     tgt_texts, _ = model.module.image_to_z(tgt_images)
             src_inputs = src_tokenizer(src_texts, padding="longest", max_length=args.max_source_length, return_tensors='pt') # ['pt', 'tf', 'np', 'jax']
-            src_texts = src_inputs['input_ids'].to(device_id, non_blocking=True)
-            src_attention_masks = src_inputs['attention_mask'].to(device_id, non_blocking=True)
+            src_texts = src_inputs['input_ids'].to(local_rank, non_blocking=True)
+            src_attention_masks = src_inputs['attention_mask'].to(local_rank, non_blocking=True)
             if args.phase == 'classify':
-                tgt_texts = tgt_texts.to(device_id, non_blocking=True)
+                tgt_texts = tgt_texts.to(local_rank, non_blocking=True)
                 tgt_attention_masks = None
             else:
                 tgt_inputs = tgt_tokenizer(tgt_texts, padding="longest", max_length=args.max_target_length, return_tensors='pt')
-                tgt_texts = tgt_inputs['input_ids'].to(device_id, non_blocking=True)
-                tgt_attention_masks = tgt_inputs['attention_mask'].to(device_id, non_blocking=True)
+                tgt_texts = tgt_inputs['input_ids'].to(local_rank, non_blocking=True)
+                tgt_attention_masks = tgt_inputs['attention_mask'].to(local_rank, non_blocking=True)
 
             loss, preds = model(src_images, src_texts, src_attention_masks, tgt_texts, tgt_attention_masks, image_mask_ratio=image_mask_ratio)
             loss /= args.accumulation_steps
@@ -109,7 +121,7 @@ def train():
             if (i + 1) % args.accumulation_steps == 0 or i + 1 == len(train_loader):
                 optimizer.step()
                 pbar.update(1)
-                if rank == 0: steps += 1
+                if world_rank == 0: steps += 1
                 if args.num_steps is not None:
                     scheduler.step()
 
@@ -119,7 +131,7 @@ def train():
         dist.all_reduce(train_count, op=dist.ReduceOp.SUM)
         pbar.close()
 
-        if rank == 0:
+        if world_rank == 0:
             train_loss /= train_count
             loss_counter.add("train", train_loss.cpu().numpy().copy())
             if args.phase == 'classify': 
@@ -135,26 +147,26 @@ def train():
         if args.language_model_train: model.module.language_model.eval()
         if args.image_model_train: model.module.image_model.eval()
         model.module.transformer.eval()
-        val_loss = torch.tensor(0.0).to(device_id)
-        if args.phase == 'classify': val_acc = torch.tensor(0.0).to(device_id)
-        val_count = torch.tensor(0).to(device_id)
-        val_loop = tqdm(val_loader, desc=f'Val (Epoch {epoch}/{args.num_epochs})', disable=(rank != 0))
+        val_loss = torch.tensor(0.0).to(local_rank)
+        if args.phase == 'classify': val_acc = torch.tensor(0.0).to(local_rank)
+        val_count = torch.tensor(0).to(local_rank)
+        val_loop = tqdm(val_loader, desc=f'Val (Epoch {epoch}/{args.num_epochs})', disable=(world_rank != 0))
         for src_images, tgt_images, src_texts, tgt_texts in val_loop:
             with torch.no_grad():
-                src_images = src_images.to(device_id, non_blocking=True)
+                src_images = src_images.to(local_rank, non_blocking=True)
                 # if args.phase == 'pretrain':
-                #    tgt_images = tgt_images.to(device_id)
+                #    tgt_images = tgt_images.to(local_rank)
                 #    tgt_texts, _ = model.module.image_to_z(tgt_images)
                 src_inputs = src_tokenizer(src_texts, padding="longest", max_length=args.max_source_length, return_tensors='pt') # ['pt', 'tf', 'np', 'jax']
-                src_texts = src_inputs['input_ids'].to(device_id, non_blocking=True)
-                src_attention_masks = src_inputs['attention_mask'].to(device_id, non_blocking=True)
+                src_texts = src_inputs['input_ids'].to(local_rank, non_blocking=True)
+                src_attention_masks = src_inputs['attention_mask'].to(local_rank, non_blocking=True)
                 if args.phase == 'classify':
-                    tgt_texts = tgt_texts.to(device_id, non_blocking=True)
+                    tgt_texts = tgt_texts.to(local_rank, non_blocking=True)
                     tgt_attention_masks = None
                 else:
                     tgt_inputs = tgt_tokenizer(tgt_texts, padding="longest", max_length=args.max_target_length, return_tensors='pt') # ['pt', 'tf', 'np', 'jax']
-                    tgt_texts = tgt_inputs['input_ids'].to(device_id, non_blocking=True)
-                    tgt_attention_masks = tgt_inputs['attention_mask'].to(device_id, non_blocking=True)
+                    tgt_texts = tgt_inputs['input_ids'].to(local_rank, non_blocking=True)
+                    tgt_attention_masks = tgt_inputs['attention_mask'].to(local_rank, non_blocking=True)
                 
                 loss, preds = model(src_images, src_texts, src_attention_masks, tgt_texts, tgt_attention_masks)
                 
@@ -167,7 +179,7 @@ def train():
         if args.phase == 'classify': dist.all_reduce(val_acc, op=dist.ReduceOp.SUM)
         dist.all_reduce(val_count, op=dist.ReduceOp.SUM)
 
-        if rank == 0:
+        if world_rank == 0:
             val_loss /= val_count
             loss_counter.add("val", val_loss.cpu().numpy().copy())
             if args.phase == 'classify':
@@ -197,7 +209,7 @@ def train():
                         torch.save(optimizer.state_dict(), os.path.join(args.result_dir, f'step_{steps}.optimizer'))
                         print(f'Model and Optimizer {steps} saved')
             
-    if rank == 0: loss_counter.plot_loss(args.result_dir)
+    if world_rank == 0: loss_counter.plot_loss(args.result_dir)
 
 if __name__=="__main__":
     train()
