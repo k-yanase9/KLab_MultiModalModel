58463cbceb36d17f5aa9bc0c5406d926658f71e1
Aboslute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-10-03 03:47:56 +0900

    不老用にpretrainのshellの変更

--- run_scripts/pretrain/train_multi.sh
+++ run_scripts/pretrain/train_multi.sh
@@ -1,12 +1,13 @@
-# 1ノード2GPU
+# 多ノード多GPU
 
-batch_size=64
+batch_size=32
 dataset="cc3m cc12m imagenet imagenet21k places365 redcaps sun397"
 
 enc=2
 dec=12
 
-/opt/conda/envs/mmm/bin/torchrun --nnodes=1 --nproc_per_node=2 train.py \
+/opt/conda/envs/mmm/bin/python train.py \
+        --multinode \
         --transformer_num_layers $enc \
         --transformer_num_decoder_layers $dec \
         --phase pretrain \
@@ -19,4 +20,4 @@ dec=12
         --datasets $dataset \
         --root_dir /data/group1/z40441a/ \
         --save_interval 1 \
-        --result_dir results/pretrain/cc3m_cc12m_imagenet_imagenet21k_places365_redcaps_sun397/enc$enc\_dec$dec/
+        --result_dir results/pretrain_multi_256/cc3m_cc12m_imagenet_imagenet21k_places365_redcaps_sun397/enc$enc\_dec$dec/
