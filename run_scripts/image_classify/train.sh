bffd6b85df69aabfa025cf424ccc85de83ca9ac5
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-10-09 14:14:02 +0900

    Update shell

--- run_scripts/image_classify/train.sh
+++ run_scripts/image_classify/train.sh
@@ -1,23 +1,21 @@
-batch_size=512
-dataset="openimage"
+batch_size=128
+dataset="sun397"
 
-epoch=50
+epoch=100
 
+enc=2
 dec=0
 lr=1e-5
-for enc in 2 4; do
-torchrun --nnodes=1 --nproc_per_node=4 train.py \
-        --language_model_name google/flan-t5-xxl \
+torchrun --nnodes=1 --nproc_per_node=1 train.py \
         --transformer_num_layers $enc \
         --transformer_num_decoder_layers $dec \
         --phase classify \
         --loss CrossEntropy \
         --lr $lr \
-        --lr_scheduler CosineAnnealingLR \
+        --lr_scheduler LambdaLR \
         -b $batch_size \
         --start_epoch 1 \
         --num_epochs $epoch \
         --datasets $dataset \
         --root_dir /local/ \
-        --result_dir results/localization/$dataset/enc$enc\_dec$dec\_xxl/cos$epoch\_$lr/
-done
+        --result_dir results/image_classify/$dataset/enc$enc\_dec$dec/Lambda$epoch\_$lr/
