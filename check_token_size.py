79276dc1f308b8fb5b60689ced903198a6d5c933
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-11-13 19:46:24 +0900

    log出力

--- check_token_size.py
+++ check_token_size.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from modules import get_logger
 from transformers import AutoTokenizer
 from data import *
 from tqdm import tqdm
@@ -18,10 +19,15 @@ parser.add_argument("--batch_size", type=int, default=512)
 parser.add_argument("--phase", type=str, default="train")
 parser.add_argument("--root_dir", type=str, default="/data01/")
 args = parser.parse_args()
-print(args)
-
 target_dataset_name = args.target_dataset_name
-print(f"Target dataset: {target_dataset_name}")
+args.result_dir = f'results/token/{target_dataset_name}'
+args.start_epoch = 1
+os.makedirs(args.result_dir, exist_ok=True)
+
+logger = get_logger(args, log_name='token_size.log')
+logger.info(args)
+
+logger.info(f"Target dataset: {target_dataset_name}")
 
 src_tokenizer = AutoTokenizer.from_pretrained(args.language_model_name, model_max_length=256, use_fast=True)
 tgt_tokenizer = AutoTokenizer.from_pretrained(args.language_model_name, model_max_length=256, use_fast=True, extra_ids=0, additional_special_tokens =[f"<extra_id_{i}>" for i in range(100)] + [f"<loc_{i}>" for i in range(args.loc_vocab_size)] + [f"<add_{i}>" for i in range(args.additional_vocab_size)])
@@ -31,7 +37,6 @@ datasets['train'] = get_dataset(args, dataset_name=target_dataset_name, phase="t
 datasets['val'] = get_dataset(args, dataset_name=target_dataset_name, phase="val")
 
 def draw_hist(score, title='', save_path='result.png'):
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
     max_score = max(score)
     plt.hist(score, bins=max_score)
     plt.title(f'{title} (max:{str(max_score)})')
@@ -41,16 +46,16 @@ def draw_hist(score, title='', save_path='result.png'):
     plt.close()
 
 for phase, dataset in datasets.items():
-    print(f"\nPhase: {phase}")
+    logger.info(f"\nPhase: {phase} ({len(dataset)} samples)")
     
     src_dataloader = np.array_split(dataset.src_texts, len(dataset.src_texts)//args.batch_size)
     tgt_dataloader = np.array_split(dataset.tgt_texts, len(dataset.tgt_texts)//args.batch_size)
-    print(f'example(src): {src_dataloader[0][0]}')
+    logger.info(f'example(src): {src_dataloader[0][0]}')
     encoded = src_tokenizer(src_dataloader[0][0], padding="longest", max_length=args.max_source_length, return_tensors="pt")
-    print(src_tokenizer.batch_decode(encoded['input_ids'][0]))
-    print(f'example(tgt): {tgt_dataloader[0][0]}')
+    logger.info(src_tokenizer.batch_decode(encoded['input_ids'][0]))
+    logger.info(f'example(tgt): {tgt_dataloader[0][0]}')
     encoded = tgt_tokenizer(tgt_dataloader[0][0], padding="longest", max_length=args.max_target_length, return_tensors="pt")
-    print(tgt_tokenizer.batch_decode(encoded['input_ids'][0]))
+    logger.info(tgt_tokenizer.batch_decode(encoded['input_ids'][0]))
 
     loop = tqdm(zip(src_dataloader, tgt_dataloader), total=len(src_dataloader), desc=f"{target_dataset_name} {phase}")
     src_counts = []
@@ -67,14 +72,14 @@ for phase, dataset in datasets.items():
         tgt_counts.extend(tgt_count.tolist())
     index = np.argmax(src_counts)
     max_text = dataset.src_texts[index]
-    print(f'max(src): {max_text}')
+    logger.info(f'max(src): {max_text}')
     encoded = src_tokenizer(max_text, padding="longest", max_length=args.max_source_length, return_tensors="pt")
-    print(src_tokenizer.batch_decode(encoded['input_ids'][0]))
+    logger.info(src_tokenizer.batch_decode(encoded['input_ids'][0]))
     index = np.argmax(tgt_counts)
     max_text = dataset.tgt_texts[index]
-    print(f'max(tgt): {max_text}')
+    logger.info(f'max(tgt): {max_text}')
     encoded = tgt_tokenizer(max_text, padding="longest", max_length=args.max_target_length, return_tensors="pt")
-    print(tgt_tokenizer.batch_decode(encoded['input_ids'][0]))
-    print(max(src_counts), max(tgt_counts))
-    draw_hist(src_counts, f'{target_dataset_name} {phase} src', f'results/token/{target_dataset_name}/{phase}_src.png')
-    draw_hist(tgt_counts, f'{target_dataset_name} {phase} tgt', f'results/token/{target_dataset_name}/{phase}_tgt.png')
+    logger.info(tgt_tokenizer.batch_decode(encoded['input_ids'][0]))
+    logger.info(f'{max(src_counts)}, {max(tgt_counts)}')
+    draw_hist(src_counts, f'{target_dataset_name} {phase} src', f'{args.result_dir}/{phase}_src.png')
+    draw_hist(tgt_counts, f'{target_dataset_name} {phase} tgt', f'{args.result_dir}/{phase}_tgt.png')
