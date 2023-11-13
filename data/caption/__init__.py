5986c049c55ed56290bf47a83e675272e77bbdf5
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-11-13 23:31:27 +0900

    vg region cap

--- data/caption/__init__.py
+++ data/caption/__init__.py
@@ -4,3 +4,4 @@ from .oidv7 import OpenImageDataset_Caption
 from .cc3m import CC3M_Caption
 from .cc12m import CC12M_Caption
 from .gtrit20m_rcap import Grit20M_RegionCaption
+from .vg_rcap import VisualGenome_RegionCaption
