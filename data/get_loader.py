086961f756acb8e4f204b189b834051e517185c1
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-11-13 22:54:33 +0900

    vg rel

--- data/get_loader.py
+++ data/get_loader.py
@@ -132,6 +132,8 @@ def get_dataset(args, dataset_name, phase="train", src_tokenizer=None, tgt_token
                 dataset = VisualGenome_Detection(data_dir, phase)
             elif 'loc' in dataset_name.lower():
                 dataset = VisualGenome_Localization(data_dir, phase)
+            elif 'rel' in dataset_name.lower():
+                dataset = VisualGenome_Relation(data_dir, phase)
             else:
                 raise NotImplementedError
         # vqa & gvqa
