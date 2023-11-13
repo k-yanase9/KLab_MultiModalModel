fb301fbd583a300800c6eb3b66d7ad98cce6990b
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-11-13 23:31:11 +0900

    tsv pathが間違っていたので修正

--- data/detection/vg_refexp.py
+++ data/detection/vg_refexp.py
@@ -13,11 +13,14 @@ class VisualGenome_RefExp(DatasetLoader):
     def __init__(self,data_dir:str="/data01/visual_genome/", phase:str="train"):
         super().__init__()        
         
-        with open(os.path.join(data_dir, f"{phase}_qa.tsv")) as f:
+        with open(os.path.join(data_dir, f"{phase}_ref_exp.tsv")) as f:
             items = f.readlines()
 
         for item in items[1:]:
-            image_id, caption, loc = item.rstrip().split("\t")
+            item = item.rstrip().split("\t")
+            if len(item) < 3:
+                continue
+            image_id, caption, loc = item
             self.images.append(os.path.join(data_dir,"images_256",f"{image_id}.png"))
             self.src_texts.append(f'Which regions does the text \“{caption}\" describe?')
             self.tgt_texts.append(loc)
