6bdb0336282284ddf9c4445d8e8f36e06b7767fe
Aboslute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-11-18 00:15:28 +0900

    best param

--- run_scripts/chech_batch/train_dummy.sh
+++ run_scripts/chech_batch/train_dummy.sh
@@ -2,9 +2,55 @@
 dataset="imagenet"
 src_len=7
 tgt_len=74
+batch_size=168
 
-for batch_size in 127; do
-torchrun --nnodes=1 --nproc_per_node=4 train_dummy.py \
+dataset="vg_det"
+src_len=8
+tgt_len=256
+batch_size=57
+
+dataset="vg_loc"
+src_len=25
+tgt_len=126
+batch_size=108
+
+dataset="cc3m"
+src_len=7
+tgt_len=256
+batch_size=56
+
+dataset="vg_rcap"
+src_len=20
+tgt_len=256
+batch_size=55
+
+dataset="vg_refexp"
+src_len=256
+tgt_len=120
+batch_size=79
+
+dataset="vg_cat"
+src_len=22
+tgt_len=17
+batch_size=246
+
+dataset="vg_rel"
+src_len=50
+tgt_len=25
+batch_size=247
+
+dataset="visual7w_vqa"
+src_len=128
+tgt_len=128
+batch_size=96
+
+dataset="visual7w_gvqa"
+src_len=102
+tgt_len=13
+batch_size=238
+
+torchrun --nnodes=1 --nproc_per_node=2 train_dummy.py \
+        --float_type float16 \
         --max_source_length $src_len \
         --max_target_length $tgt_len \
         --phase train \
@@ -14,4 +60,3 @@ torchrun --nnodes=1 --nproc_per_node=4 train_dummy.py \
         --datasets $dataset \
         --root_dir /data01/ \
         --result_dir results/check_batch/$dataset/
-done
