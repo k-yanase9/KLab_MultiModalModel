5f88a286f11ea05d0b10220bd949bb2ca4a99427
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-11-29 18:45:06 +0900

    5iterでも動く

--- check_batch_size.py
+++ check_batch_size.py
@@ -16,8 +16,8 @@ from modules import *
 ONE_GPU_BATCH_DICT = {"caption": 48, "relation":192, "rcap":48, "refexp":72, "det":48, "cat":192, "loc":96, "vqa": 72, "gvqa":48, "classify": 144} #1gpuのバッチサイズ
 TASK_SAMPLE_NUM_DICT = {"caption": 12, "relation":3, "rcap":12, "refexp":8, "det":12, "cat":3, "loc":6, "vqa": 8, "gvqa":2, "classify": 4} #何回タスクごとにバッチを取得するか
 # H100
-ONE_GPU_BATCH_DICT = {"caption": 120, "relation":480, "rcap":120, "refexp":180, "det":120, "cat":480, "loc":240, "vqa": 180, "gvqa":210, "classify": 360} #1gpuのバッチサイズ
-TASK_SAMPLE_NUM_DICT = {"caption": 12, "relation":3, "rcap":12, "refexp":8, "det":12, "cat":3, "loc":6, "vqa": 8, "gvqa":1, "classify": 4} #何回タスクごとにバッチを取得するか
+ONE_GPU_BATCH_DICT = {"caption": 120, "relation":360, "rcap":120, "refexp":180, "det":120, "cat":360, "loc":240, "vqa": 180, "gvqa":210, "classify": 360} #1gpuのバッチサイズ
+TASK_SAMPLE_NUM_DICT = {"caption": 12, "relation":4, "rcap":12, "refexp":8, "det":12, "cat":4, "loc":6, "vqa": 8, "gvqa":1, "classify": 4} #何回タスクごとにバッチを取得するか
 # General
 SRC_LEN_DICT = {"caption": 7, "relation":50, "rcap":20, "refexp":184, "det":8, "cat":22, "loc":25, "vqa": 125, "gvqa":256, "classify": 7}
 TGT_LEN_DICT = {"caption": 256, "relation":25, "rcap":256, "refexp":120, "det":256, "cat":17, "loc":126, "vqa": 128, "gvqa":103, "classify": 74}
@@ -116,13 +116,13 @@ def train():
     if args.language_model_train: model.module.language_model.train()
     if args.image_model_train: model.module.image_model.train()
     model.module.transformer.train()
-    train_loss = torch.tensor(0.0).to(local_rank)
-    train_count = torch.tensor(0).to(local_rank)
+    train_loss = 0.0
+    train_count = 0
     
     for iter in range(5):
-        accumulation_sample_size = torch.tensor(0).long().to(local_rank)
+        accumulation_sample_size = torch.tensor(0).to(local_rank)
+        loss_per_step = torch.tensor(0.0).to(local_rank)
         for task in ONE_GPU_BATCH_DICT.keys():
-            loss_per_step = 0
             #累積数分の使用するデータをモデルに通して、勾配累積
             batch_size = ONE_GPU_BATCH_DICT[task]
             max_src_len = SRC_LEN_DICT[task]
@@ -136,13 +136,12 @@ def train():
                 tgt_attention_masks = torch.ones_like(tgt_texts, device=local_rank, dtype=torch.bool)
                 src_attention_masks = torch.ones_like(src_texts, device=local_rank, dtype=torch.bool)
 
-                loss, preds, sample_size = model(src_images, src_texts, src_attention_masks, tgt_texts, tgt_attention_masks)
+                loss, _, sample_size = model(src_images, src_texts, src_attention_masks, tgt_texts, tgt_attention_masks)
                 loss_per_step += loss.item()
-                accumulation_sample_size += sample_size
+                accumulation_sample_size += sample_size.item()
                 scaler.scale(loss).backward()
 
                 train_loss += loss.item() #loss.item() * src_images.shape[0]
-                #train_count += src_images.shape[0]
 
         # if (i + 1) % args.accumulation_steps == 0 or i + 1 == len(train_loader):
         #sum_loss/num_tokens
@@ -153,9 +152,11 @@ def train():
         multiply_grad(optimizer, grad_scale)
         
         #記録準備
-        loss_per_step = torch.tensor(loss_per_step).to(local_rank)
         dist.all_reduce(loss_per_step, op=dist.ReduceOp.SUM)
-        train_count += accumulation_sample_size
+        loss_per_step /= accumulation_sample_size
+
+        train_loss += loss_per_step.cpu().numpy().copy()
+        train_count += accumulation_sample_size.cpu().numpy().copy()
         
         #勾配更新
         scaler.step(optimizer)
@@ -168,17 +169,13 @@ def train():
         if world_rank == 0:
             steps += 1
             if use_wandb:
-                wandb.log({"iter": steps, "iter/loss": loss_per_step.item()/accumulation_sample_size.item(), "iter/lr": optimizer.param_groups[0]["lr"]})
+                wandb.log({"iter": steps, "iter/loss": loss_per_step.item(), "iter/lr": optimizer.param_groups[0]["lr"]})
         if args.num_steps is not None:
             scheduler.step()
 
-    # 他のノードから集める
-    dist.all_reduce(train_loss, op=dist.ReduceOp.SUM)
-    # dist.all_reduce(train_count, op=dist.ReduceOp.SUM)
-
     if world_rank == 0:
         train_loss /= train_count
-        loss_counter.add("train", train_loss.cpu().numpy().copy())
+        loss_counter.add("train", train_loss)
         logger.info(f'[Epoch ({epoch}/{args.num_epochs}) Train] Loss : {train_loss}, Steps : {steps}, LR : {optimizer.param_groups[0]["lr"]}')
         if use_wandb:
             wandb.log({"epoch": epoch, "train/loss": train_loss, "train/lr": optimizer.param_groups[0]["lr"]})
@@ -192,8 +189,8 @@ def train():
     if args.image_model_train:
         model.module.image_model.eval()
     model.module.transformer.eval()
-    val_loss = torch.tensor(0.0).to(local_rank)
-    val_count = torch.tensor(0).to(local_rank)
+    loss_per_step = torch.tensor(0.0).to(local_rank)
+    accumulation_sample_size = torch.tensor(0).to(local_rank)
     val_loop = tqdm(range(val_iter_per_epoch), desc=f'Val (Epoch {epoch}/{args.num_epochs})', disable=(world_rank != 0))
     for i in val_loop:
         #勾配更新の前準備
@@ -207,16 +204,16 @@ def train():
 
             loss, preds, sample_size = model(src_images, src_texts, src_attention_masks, tgt_texts, tgt_attention_masks)
 
-            val_loss += loss.item()
-            val_count += sample_size
+            loss_per_step += loss.item()
+            accumulation_sample_size += sample_size
 
     # 他のノードから集める
-    dist.all_reduce(val_loss, op=dist.ReduceOp.SUM)
-    dist.all_reduce(val_count, op=dist.ReduceOp.SUM)
+    dist.all_reduce(loss_per_step, op=dist.ReduceOp.SUM)
+    dist.all_reduce(accumulation_sample_size, op=dist.ReduceOp.SUM)
 
     if world_rank == 0:
-        val_loss /= val_count
-        loss_counter.add("val", val_loss.cpu().numpy().copy())
+        val_loss = (loss_per_step / accumulation_sample_size).cpu().numpy().copy()
+        loss_counter.add("val", val_loss)
         logger.info(f'[Epoch ({epoch}/{args.num_epochs}) Val] Loss : {val_loss}')
         if use_wandb:
             wandb.log({"epoch": epoch, "val/loss": val_loss})
