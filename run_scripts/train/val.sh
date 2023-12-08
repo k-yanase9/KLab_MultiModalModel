59d6bc58a502f69296cb51dec366968f3fc57335
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-12-08 19:13:10 +0900

    val shell

--- run_scripts/train/val.sh
+++ run_scripts/train/val.sh
@@ -1,7 +1,7 @@
 # 1ノード8GPU
 
 # Linear
-batch_size=256
+batch_size=300
 dataset="all"
 
 epoch=50
@@ -19,7 +19,7 @@ torchrun --nnodes=1 --nproc_per_node=8 multi_task_val.py \
         -b $batch_size \
         --start_epoch 1 \
         --num_epochs $epoch \
-        --warmup_rate 0.001 \
+        --warmup_rate 0.01 \
         --datasets $dataset \
         --root_dir /local/ \
         --result_dir results/train/all/enc$enc\_dec$dec/Linear$epoch\_$lr/
