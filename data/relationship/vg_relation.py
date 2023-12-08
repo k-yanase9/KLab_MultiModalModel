44d52aa808eca087762bc38e60ba4dd5edd71991
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-12-08 18:01:54 +0900

    val relation

--- data/relationship/vg_relation.py
+++ data/relationship/vg_relation.py
@@ -1,22 +1,27 @@
 import os
-from ..dataset_loader import DatasetLoader
+from ..dataset_loader import DatasetLoader, MAX_VAL_DATA_SIZE
 
 class VisualGenome_Relation(DatasetLoader):
     """VisualGenomeのrelationデータセット
     """    
     def __init__(self, data_dir:str="/data01/visual_genome/", phase:str="train", **kwargs):
-        super().__init__(**kwargs)        
+        super().__init__(**kwargs)
+        tsv_path = os.path.join(data_dir, f"{phase}_relation.tsv")
+        
+        with open(tsv_path) as f:
+            lines = f.readlines()
+        lines = lines[1:]
+        count = 0
 
-        with open(os.path.join(data_dir,f"{phase}_relation.tsv")) as f:
-            items = f.readlines()
-        tmp = []
-        for item in items:
-            item = item.rstrip().split("\t")
-            if len(item) < 6:
+        for line in lines:
+            if count >= MAX_VAL_DATA_SIZE and phase == 'val':
+                break
+            line = line.removesuffix('\n').split('\t')
+            if len(line) < 6:
                 continue
-            tmp.append(item)
-        items = tmp[1:]
-
-        self.tgt_texts = [f"{item[1]} {item[5]} {item[3]}" for item in items]
-        self.src_texts = [f"What is the relationship between {item[1]}{item[2]} and {item[3]}{item[4]}?" for item in items]
-        self.images = [os.path.join(data_dir,"images_256",f"{item[0]}.png") for item in items]
+            img_name, obj1, loc1, obj2, loc2, relation = line
+            img_path = os.path.join(data_dir, f"images_256", f"{img_name}.png")
+            self.images.append(img_path)
+            self.src_texts.append(f"What is the relationship between {obj1}{loc1} and {obj2}{loc2}?")
+            self.tgt_texts.append(f"{obj1} {relation} {obj2}")
+            count += 1
