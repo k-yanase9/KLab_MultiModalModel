14f3508170068e69443d8baa73a939df7b7a27b3
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-12-12 15:38:30 +0900

    Update pretrain

--- data/pretrain/cc12m.py
+++ data/pretrain/cc12m.py
@@ -4,19 +4,9 @@ from .pretrain import PretrainDatasetLoader
 class CC12M_Pretrain(PretrainDatasetLoader):
     def __init__(self, data_dir='/data01/cc12m/', phase='train', **kwargs):
         super().__init__(**kwargs)
-        text_tsv_path = os.path.join(data_dir, f'text_{phase}.tsv')
-        img_tsv_path = os.path.join(data_dir, f'img_{phase}.tsv')
+        tsv_path = os.path.join(data_dir, f'{phase}_cut_max_tokens.tsv')
 
-        with open(text_tsv_path, 'r') as f:
-            lines = f.readlines()
-
-        for line in lines[1:]:
-            img_name, caption = line.removesuffix('\n').split('\t')
-            img_path = os.path.join(data_dir, 'images', img_name)
-            self.images.append(img_path)
-            self.src_texts.append(caption)
-
-        with open(img_tsv_path, 'r') as f:
+        with open(tsv_path, 'r') as f:
             lines = f.readlines()
 
         for line in lines[1:]:
