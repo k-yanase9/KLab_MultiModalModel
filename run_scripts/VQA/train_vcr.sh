a0578bdc22bfac74a74ad4a805f6edfbe60f9d49
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2023-12-13 17:22:12 +0900

    remove unuse shell

--- run_scripts/VQA/train_vcr.sh
+++
@@ -1,7 +0,0 @@
-torchrun --nnodes=1 --nproc_per_node=4 train.py \
- --data_dir /data/dataset/vcr  \
- --image_model_train \
- --result_dir results/qva/vcr/ \
- --batch_size 8 \
- --num_epochs 30 \
- --save_interval 5
