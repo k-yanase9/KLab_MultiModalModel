997ac40c28b59ca266417ea5df012dd64a9692e3
k-yanase <k-yanase@cv.info.gifu-u.ac.jp>
2024-03-08 08:31:08 +0900

    add 4090で回る定義を追加

--- multi_task_train.py
+++ multi_task_train.py
@@ -26,14 +26,21 @@ FULL_DATASET_NAME_DICT = {
     "gvqa": ["vcr", "visual7w_gvqa"],
     "classify": ["imagenet", "imagenet21k", "places365", "sun397"]
 }
-# Flow
-ONE_GPU_BATCH_DICT = {"caption": 48, "relation":144, "rcap":48, "refexp":72, "det":48, "cat":72, "loc":96, "vqa": 72, "gvqa":48, "classify": 144} #1gpuのバッチサイズ
+# # Flow
+# ONE_GPU_BATCH_DICT = {"caption": 48, "relation":144, "rcap":48, "refexp":72, "det":48, "cat":72, "loc":96, "vqa": 72, "gvqa":48, "classify": 144} #1gpuのバッチサイズ
+# TASK_SAMPLE_NUM_DICT = {"caption": 6, "relation":2, "rcap":6, "refexp":4, "det":6, "cat":2, "loc":3, "vqa": 4, "gvqa":1, "classify": 2} #何回タスクごとにバッチを取得するか
+# NUM_STEP_PER_EPOCH_MAX = 5120
+# # H100
+# ONE_GPU_BATCH_DICT = {"caption": 120, "relation":360, "rcap":90, "refexp":180, "det":120, "cat":360, "loc":240, "vqa": 180, "gvqa":125, "classify": 360} #1gpuのバッチサイズ
+# TASK_SAMPLE_NUM_DICT = {"caption": 6, "relation":2, "rcap":6, "refexp":4, "det":6, "cat":2, "loc":3, "vqa": 4, "gvqa":1, "classify": 2} #何回タスクごとにバッチを取得するか
+# NUM_STEP_PER_EPOCH_MAX = 2400
+
+# 4090
+ONE_GPU_BATCH_DICT = {"caption": 20, "relation":60, "rcap":15, "refexp":30, "det":20, "cat":60, "loc":40, "vqa": 30, "gvqa":20, "classify": 60} #1gpuのバッチサイズ
 TASK_SAMPLE_NUM_DICT = {"caption": 6, "relation":2, "rcap":6, "refexp":4, "det":6, "cat":2, "loc":3, "vqa": 4, "gvqa":1, "classify": 2} #何回タスクごとにバッチを取得するか
-NUM_STEP_PER_EPOCH_MAX = 5120
-# H100
-ONE_GPU_BATCH_DICT = {"caption": 120, "relation":360, "rcap":90, "refexp":180, "det":120, "cat":360, "loc":240, "vqa": 180, "gvqa":125, "classify": 360} #1gpuのバッチサイズ
-TASK_SAMPLE_NUM_DICT = {"caption": 6, "relation":2, "rcap":6, "refexp":4, "det":6, "cat":2, "loc":3, "vqa": 4, "gvqa":1, "classify": 2} #何回タスクごとにバッチを取得するか
-NUM_STEP_PER_EPOCH_MAX = 2400
+NUM_STEP_PER_EPOCH_MAX = 13200
+
+
 # General
 SRC_LEN_DICT = {"caption": 7, "relation":50, "rcap":20, "refexp":184, "det":8, "cat":22, "loc":25, "vqa": 125, "gvqa":256, "classify": 7}
 TGT_LEN_DICT = {"caption": 256, "relation":25, "rcap":256, "refexp":120, "det":256, "cat":17, "loc":126, "vqa": 128, "gvqa":103, "classify": 18}
--- run_scripts/train/train_yanase.sh
+++ run_scripts/train/train_yanase.sh
@@ -1,8 +1,8 @@
-# 1ノード8GPU
+# 1ノード2GPU
 
 # Linear
 batch_size=400
-dataset="all"
+dataset="vqa"
 
 epoch=50
 
@@ -10,19 +10,21 @@ enc=2
 dec=12
 lr=1e-4
 
-torchrun --nnodes=1 --nproc_per_node=8 multi_task_train.py \
+torchrun --nnodes=1 --nproc_per_node=2 multi_task_train.py \
+        --transformer_d_ff 2048 \
         --transformer_num_layers $enc \
         --transformer_num_decoder_layers $dec \
+        --transformer_model_init pretrain \
         --stage train \
         --loss CrossEntropy \
         --lr $lr \
         --lr_scheduler LinearWarmup \
         -b $batch_size \
-        --start_epoch 2 \
+        --start_epoch 1 \
         --num_epochs $epoch \
         --warmup_rate 0.01 \
         --datasets $dataset \
-        --root_dir /local/ \
+        --root_dir /data01/ \
         --uncalc_val \
         --save_interval 1 \
-        --result_dir results/train/all/enc$enc\_dec$dec/Linear$epoch\_$lr/
+        --result_dir results/train/vqa/enc$enc\_dec$dec/Linear$epoch\_$lr/
